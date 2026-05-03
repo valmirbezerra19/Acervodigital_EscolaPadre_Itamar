80aeb969567ed7781387923a4e8aaf8fb228e16a
Valmir Aparecido Bezerra <92387164+valmirbezerra19@users.noreply.github.com>
2026-05-03 00:40:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ const EVENTOS_ESCOLARES = [
   { data: "2026-02-09", titulo: "Início do Ano Letivo", cat: "ACADÊMICO" },
   { data: "2026-03-27", titulo: "Reunião Pedagógica", cat: "PEDAGÓGICO" },
   { data: "2026-05-10", titulo: "Homenagem Dia das Mães", cat: "SOCIAL" },
-    { data: "2026-06-20", titulo: "Festa Junina", cat: "EVENTO" },
+  { data: "2026-06-20", titulo: "Festa Junina", cat: "EVENTO" },
   { data: "2026-06-20", titulo: "Festa Junina", cat: "EVENTO" },
   { data: "2026-09-07", titulo: "Desfile de Independência", cat: "CÍVICO" },
   { data: "2026-11-20", titulo: "Mostra Cultural 50 Anos", cat: "CULTURAL" },
